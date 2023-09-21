5c2e2145bf0766ecd922a89fda4f7189251d74a5
daydreaming <dd@daydreamingdeMacBook-Pro.local>
2023-09-21 23:45:13 +0800

    update

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,19 @@
-console.log($request.url)
-console.log($request.method)
-console.log($response.body)
+const keys = Object.keys($response.body)
+if (keys.length == 0 || keys[0] != '1803803057') {
+    $done({})
+}
 
-$done({})
+console.log('修改中')
+
+const body = $response.body
+const info = body[keys[0]]
+info['position'] = '北京市 容达路7号院3号楼太极信息产业园 2层'
+
+$done({
+    body: {
+        "data": {
+            '1803803057': info,
+        }
+    }
+})
+
