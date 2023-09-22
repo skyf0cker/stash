a9e4af8a43be1deb706b105f8834899eb28584a8
dd <dev@vophan.day>
2023-09-22 10:22:46 +0800

    update

--- scripts/hijack-vcard.js
+++ scripts/hijack-vcard.js
@@ -1,40 +1,32 @@
-const resp = JSON.parse($response.body)
-console.log(resp)
-console.log(resp['rescode'])
-console.log(resp['data'])
+(function () {
+    const resp = JSON.parse($response.body);
+    const rescode = resp["rescode"];
+    if (rescode != 0) {
+        console.log("error rescode: ", rescode);
+        $done({});
+        return;
+    }
 
-// const rescode = resp['rescode']
-// if (rescode != 0) {
-//     console.log('error rescode: ', rescode)
-//     $done({})
-//     return
-// }
-//
-// const data = resp['data']
-//
-// console.log('data ', data)
-//  
-// const keys = Object.keys(data)
-//
-// console.log('keys ', keys)
-// if (keys.length == 0 || keys[0] != '1803803057') {
-//     $done({})
-//     return
-// }
-//
-// console.log('修改中')
-//
-// const info = data[keys[0]]
-// info['position'] = '北京市 容达路7号院3号楼太极信息产业园 2层'
-//
-// console.log(info)
-//
-// $done({
-//     body: {
-//         "data": {
-//             '1803803057': info,
-//         },
-//         "rescode": 0,
-//     }
-// })
-//
+    const data = resp["data"];
+    const id = "1803803057"
+    if (!(id in data)) {
+        $done({});
+        return;
+    }
+
+    console.log("修改中");
+
+    const info = data[id];
+    info["position"] = "北京市 容达路7号院3号楼太极信息产业园 2层";
+
+    console.log(info);
+
+    $done({
+        body: {
+            data: {
+                1803803057: info,
+            },
+            rescode: 0,
+        },
+    });
+})();
