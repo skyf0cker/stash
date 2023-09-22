98e26e23e76144369be71b6414875f6a0e49d461
daydreaming <dd@daydreamingdeMacBook-Pro.local>
2023-09-22 08:53:51 +0800

    update

--- scripts/hijack-vcard.js
+++ scripts/hijack-vcard.js
@@ -1,38 +1,38 @@
 const resp = $response.body
 console.log(resp, typeof resp)
 
-const rescode = resp['rescode']
-if (rescode != 0) {
-    console.log('error rescode: ', rescode)
-    $done({})
-    return
-}
-
-const data = resp['data']
-
-console.log('data ', data)
- 
-const keys = Object.keys(data)
-
-console.log('keys ', keys)
-if (keys.length == 0 || keys[0] != '1803803057') {
-    $done({})
-    return
-}
-
-console.log('修改中')
-
-const info = data[keys[0]]
-info['position'] = '北京市 容达路7号院3号楼太极信息产业园 2层'
-
-console.log(info)
-
-$done({
-    body: {
-        "data": {
-            '1803803057': info,
-        },
-        "rescode": 0,
-    }
-})
-
+// const rescode = resp['rescode']
+// if (rescode != 0) {
+//     console.log('error rescode: ', rescode)
+//     $done({})
+//     return
+// }
+//
+// const data = resp['data']
+//
+// console.log('data ', data)
+//  
+// const keys = Object.keys(data)
+//
+// console.log('keys ', keys)
+// if (keys.length == 0 || keys[0] != '1803803057') {
+//     $done({})
+//     return
+// }
+//
+// console.log('修改中')
+//
+// const info = data[keys[0]]
+// info['position'] = '北京市 容达路7号院3号楼太极信息产业园 2层'
+//
+// console.log(info)
+//
+// $done({
+//     body: {
+//         "data": {
+//             '1803803057': info,
+//         },
+//         "rescode": 0,
+//     }
+// })
+//
