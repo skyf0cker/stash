e5699052d121fb182c8c03718d5b26be6c3ed33b
daydreaming <dd@daydreamingdeMacBook-Pro.local>
2023-09-24 01:21:14 +0800

    update

--- scripts/checkin-tile.js
+++ scripts/checkin-tile.js
@@ -1,4 +1,18 @@
 (function () {
-    console.log($script.startTime, typeof $script.startTime);
-    $done({});
+    let FORMAT_TIME = "yyyy-MM-dd hh:mm:ss";
+    const startTime = $script.startTime;
+    console.log(startTime.format(FORMAT_TIME));
+
+    const weekday = startTime.getDay();
+    if (weekday == 0 || weekday == 6) {
+        $done({
+            content: "周末了，休息一下",
+            backgroundColor: "#2BEC17",
+        });
+        return;
+    }
+
+    $done({
+        content: startTime.format(FORMAT_TIME),
+    });
 })();
