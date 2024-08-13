4818f7b4b3afc8dd3a6e9d2bbe3454243d77ff5b
l4m4r <dev@vophan.day>
2024-08-13 18:58:18 +0800

    update

--- scripts/floatingisland.js
+++ scripts/floatingisland.js
@@ -1,9 +1,7 @@
 (function () {
   const resp = JSON.parse($response.body);
-  const request = JSON.parse($request.body);
   console.log($request.url);
   console.log($request.method);
-  console.log(request);
   console.log(resp);
   $done({});
 })();
